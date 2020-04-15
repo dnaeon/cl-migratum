53315c7400d6a43b0beca1cd4dded2996de17a1b
Marin Atanasov Nikolov <dnaeon@gmail.com>
2020-04-15 16:14:54 +0300

    core: add accessors to base class slots

--- src/core.lisp
+++ src/core.lisp
@@ -4,11 +4,17 @@
   (:nicknames :migratum.core)
   (:export
    :migration
+   :migration-id
+   :migration-description
+   :migration-applied
    :base-provider
+   :provider-name
    :list-migrations
    :load-migration
    :create-migration
    :base-driver
+   :driver-name
+   :driver-provider
    :init
    :applied
    :register
@@ -19,20 +25,24 @@
   ((id
     :type integer
     :initarg :id
-    :initform (error "Must specify migration id"))
+    :initform (error "Must specify migration id")
+    :accessor migration-id)
    (description
     :type string
     :initarg :description
-    :initform (error "Must specify migration description"))
+    :initform (error "Must specify migration description")
+    :accessor migration-description)
    (applied
-    :initarg :applied))
+    :initarg :applied
+    :accessor migration-applied))
   (:documentation "Base class for migration resources"))
 
 (defclass base-provider ()
   ((name
     :type string
     :initarg :name
-    :initform (error "Must specify provider name")))
+    :initform (error "Must specify provider name")
+    :accessor provider-name))
   (:documentation "Base class for migration providers"))
 
 (defgeneric list-migrations (provider &key)
@@ -41,17 +51,19 @@
 (defgeneric load-migration (provider migration &key)
   (:documentation "Returns the contents of the migration resource using the given provider"))
 
-(defgeneric create-migration (provider &optional id description content)
+(defgeneric create-migration (provider &key id description content)
   (:documentation "Creates a new migration resource using the given provider"))
 
 (defclass base-driver ()
   ((name
     :type string
     :initarg :name
-    :initform (error "Must specify driver name"))
+    :initform (error "Must specify driver name")
+    :accessor driver-name)
    (provider
     :initarg :provider
-    :initform (error "Must specify migrations provider")))
+    :initform (error "Must specify migrations provider")
+    :accessor driver-provider))
   (:documentation "Base class for migration drivers"))
 
 (defgeneric init (driver &key)
