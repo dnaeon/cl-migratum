e504c15b18b0fd1fe07c825557a0b1a67c98550e
Marin Atanasov Nikolov <dnaeon@gmail.com>
2020-04-15 13:35:05 +0300

    core: define generic methods for providers
    A `provider` provides migration resources, e.g.
    SQL files which reside on a local filesystem or a
    remote endpoint.

--- src/core.lisp
+++ src/core.lisp
@@ -3,7 +3,11 @@
   (:use :cl)
   (:nicknames :migratum.core)
   (:export
-   :migration))
+   :migration
+   :base-provider
+   :list-migrations
+   :load-migration
+   :create-migration))
 (in-package :cl-migratum.core)
 
 (defclass migration ()
@@ -18,3 +22,19 @@
    (applied
     :initarg :applied))
   (:documentation "Base class for migration resources"))
+
+(defclass base-provider ()
+  ((name
+    :type string
+    :initarg :name
+    :initform (error "Must specify provider name")))
+  (:documentation "Base class for migration providers"))
+
+(defgeneric list-migrations (provider &key)
+  (:documentation "Returns the list of migration resources discovered by the provider"))
+
+(defgeneric load-migration (provider migration &key)
+  (:documentation "Returns the contents of the migration resource using the given provider"))
+
+(defgeneric create-migration (provider &optional id description content)
+  (:documentation "Creates a new migration resource using the given provider"))
