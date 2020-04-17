8dfb365b95751fa681a4cb29eeab8c088151b860
Marin Atanasov Nikolov <dnaeon@gmail.com>
2020-04-17 23:08:06 +0300

    driver.sql: implement generic methods for applying up and down migrations

--- src/driver/sql.lisp
+++ src/driver/sql.lisp
@@ -11,17 +11,21 @@
    :driver-name
    :driver-init
    :driver-provider
-   :apply-migration
+   :apply-up-migration
+   :apply-down-migration
    :list-applied
    :list-pending
    :register-migration
-   :load-migration)
+   :unregister-migration
+   :load-migration-up-script
+   :load-migration-down-script)
   (:import-from :log)
   (:import-from :cl-dbi)
   (:export
    :sql-driver
    :sql-driver-connection
-   :make-sql-driver))
+   :make-sql-driver
+   :sql-driver-apply-migration))
 (in-package :cl-migratum.driver.sql)
 
 (defparameter *sql-init-schema*
@@ -69,13 +73,21 @@ CREATE TABLE IF NOT EXISTS migration (
     (cl-dbi:with-transaction connection
       (cl-dbi:execute query (list id description)))))
 
-(defmethod apply-migration ((driver sql-driver) (migration migration) &key)
-  (log:debug "Applying migration: ~a - ~a" (migration-id migration) (migration-description migration))
+(defmethod unregister-migration ((driver sql-driver) (migration migration) &key)
+  (log:debug "Unregistering migration: ~a" (migration-id migration))
   (let* ((connection (sql-driver-connection driver))
-         (content (load-migration migration))
-         (query (cl-dbi:prepare connection (string-trim #(#\Newline) content))))
+	 (id (migration-id migration))
+	 (query (cl-dbi:prepare connection "DELETE FROM migration WHERE id = ?")))
     (cl-dbi:with-transaction connection
-      (cl-dbi:execute query))))
+      (cl-dbi:execute query (list id)))))
+
+(defmethod apply-up-migration ((driver sql-driver) (migration migration) &key)
+  (log:debug "Applying upgrade migration: ~a - ~a" (migration-id migration) (migration-description migration))
+  (sql-driver-apply-migration driver migration #'load-migration-up-script))
+
+(defmethod apply-down-migration ((driver sql-driver) (migration migration) &key)
+  (log:debug "Applying downgrade migration: ~a - ~a" (migration-id migration) (migration-description migration))
+  (sql-driver-apply-migration driver migration #'load-migration-down-script))
 
 (defun make-sql-driver (provider connection)
   "Creates a driver for performing migrations against a SQL database"
@@ -83,3 +95,11 @@ CREATE TABLE IF NOT EXISTS migration (
                  :name "SQL"
                  :provider provider
                  :connection connection))
+
+(defun sql-driver-apply-migration (driver migration migration-script-loader-fun)
+  "Applies the script loaded using the migration script loader function"
+  (let* ((connection (sql-driver-connection driver))
+	 (content (funcall migration-script-loader-fun migration))
+	 (query (cl-dbi:prepare connection (string-trim #(#\Newline) content))))
+    (cl-dbi:with-transaction connection
+      (cl-dbi:execute query))))
