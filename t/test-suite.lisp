80abe6c2d739de0d4973cc7757b35f37834b9d1e
Marin Atanasov Nikolov <dnaeon@gmail.com>
2020-04-27 17:33:14 +0300

    tests: add tests for local-path-provider

--- t/test-suite.lisp
+++ t/test-suite.lisp
@@ -5,10 +5,21 @@
   (:import-from :tmpdir)
   (:import-from :asdf)
   (:import-from :cl-dbi)
-  (:import-from :migratum)
+  (:import-from
+   :migratum
+   :provider-init
+   :provider-initialized
+   :provider-list-migrations
+   :provider-create-migration
+   :migration-id
+   :migration-description
+   :migration-load-up-script
+   :migration-load-down-script)
   (:import-from
    :migratum.provider.local-path
-   :make-local-path-provider)
+   :make-local-path-provider
+   :local-path-migration-up-script-path
+   :local-path-migration-down-script-path)
   (:import-from
    :migratum.driver.sql
    :make-sql-driver))
@@ -46,3 +57,33 @@
 (teardown
   (when *tmpdir*
     (uiop:delete-directory-tree *tmpdir* :validate t)))
+
+(deftest local-path-provider
+  (testing "provider-init"
+    (ok (eq t (provider-init *provider*))))
+
+  (testing "provider-initialized"
+    (ok (eq t (provider-initialized *provider*))))
+
+  (testing "provider-list-migrations"
+    (let* ((discovered (provider-list-migrations *provider*))
+           (migrations (sort discovered #'< :key #'migration-id)))
+      (ok (= 3 (length migrations)))
+      (ok (equal (list 20200421173657 20200421173908 20200421180337)
+                 (mapcar #'migration-id migrations)))
+      (ok (equal (list "create-table-foo" "create-table-bar" "create-table-qux")
+                 (mapcar #'migration-description migrations)))))
+
+  (testing "provider-create-migration"
+    (let* ((migration (provider-create-migration *provider*
+						 :description "my-new-migration"
+						 :up "CREATE TABLE cl_migratum_test (id INTEGER PRIMARY KEY);"
+						 :down "DROP TABLE cl_migratum_test;")))
+      (ok (numberp (migration-id migration)))
+      (ok (string= "my-new-migration" (migration-description migration)))
+      (ok (string= "CREATE TABLE cl_migratum_test (id INTEGER PRIMARY KEY);"
+                   (migration-load-up-script migration)))
+      (ok (string= "DROP TABLE cl_migratum_test;"
+                   (migration-load-down-script migration)))
+      (uiop:delete-file-if-exists (local-path-migration-up-script-path migration))
+      (uiop:delete-file-if-exists (local-path-migration-down-script-path migration)))))
