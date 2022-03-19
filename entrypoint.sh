b1c2a6543a9dff34952e965e5471178ffb10c8a6
Kambiz Darabi <darabi@m-creations.net>
2022-03-19 06:59:48 +0100

    Use Docker image clfoundation/sbcl during tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sbcl --eval '(ql:quickload :cl-migratum.test)' \
+sbcl --non-interactive --eval '(ql:quickload :cl-migratum.test)' \
      --eval '(setf rove:*enable-colors* nil)' \
      --eval '(asdf:test-system :cl-migratum.test)' \
-     --eval '(quit)'
+     --eval '(uiop:quit (length (rove/core/stats:all-failed-assertions rove/core/stats:*stats*)))'
