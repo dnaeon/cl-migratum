f016fbc9cd33316aab7e1e9b00f8de872024e4b5
Marin Atanasov Nikolov <dnaeon@gmail.com>
2022-03-23 12:33:06 +0200

    Bump license headers

--- src/driver/dbi.lisp
+++ src/driver/dbi.lisp
@@ -1,4 +1,4 @@
-;; Copyright (c) 2020 Marin Atanasov Nikolov <dnaeon@gmail.com>
+;; Copyright (c) 2020-2022 Marin Atanasov Nikolov <dnaeon@gmail.com>
 ;; All rights reserved.
 ;;
 ;; Redistribution and use in source and binary forms, with or without
