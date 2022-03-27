0b5ebe640437bcfa23630b9a9bbfae8b85ac86dd
Marin Atanasov Nikolov <dnaeon@gmail.com>
2022-03-27 12:40:27 +0300

    provider.local-path: add pattern and mappings to local-path-provider
    Lay down the foundations needed to be able to discover new migration
    kinds.

--- src/provider/local-path.lisp
+++ src/provider/local-path.lisp
@@ -41,21 +41,20 @@
    :provider-create-migration
    :make-migration-id)
   (:export
-   :*migration-file-regex*
    :migration-file-p
    :find-migration-files
    :normalize-description
    :local-path-provider
    :local-path-migration
+   :get-file-mapping
+   :sql-migration
    :migration-up-script-path
    :migration-down-script-path
    :provider-paths
-   :make-local-path-provider))
+   :provider-scan-pattern
+   :provider-file-mappings
+   :make-provider))
 (in-package :cl-migratum.provider.local-path)
-
-(defparameter *migration-file-regex*
-  "(\\d{14})-(.*)\.(up|down)\.sql$"
-  "Regex used to match migration files")
 
 (defclass local-path-migration (base-migration)
   ((up-script-path
@@ -74,7 +73,7 @@
   ()
   (:default-initargs
    :kind :sql)
-  (:documentation "Migration resource from local SQL path"))
+  (:documentation "SQL migration resource"))
 
 (defmethod migration-load ((direction (eql :up)) (migration sql-migration))
   "Loads the upgrade SQL migration"
@@ -90,20 +89,41 @@
     (log:debug "[SQL] Loading downgrade migration for id ~a from ~a" id path)
     (uiop:read-file-string path)))
 
+(defgeneric get-file-mapping (provider extension)
+  (:documentation "Returns the mapping associated with the given file EXTENSION"))
+
 (defclass local-path-provider (base-provider)
   ((paths
     :initarg :paths
     :initform (error "Must specify migration resource paths")
     :reader provider-paths
-    :documentation "Local paths from which to discover migrations"))
-  (:documentation "Provider for discovering migrations from a local path"))
+    :documentation "Local paths from which to discover migrations")
+   (pattern
+    :initarg :pattern
+    :initform "(\\d{14})-(.*)\.(up|down)\.(sql)$"
+    :reader provider-scan-pattern
+    :documentation "Regex pattern used to scan for migration files")
+   (mappings
+    :initarg :mappings
+    :reader provider-file-mappings
+    :initform `((:extension "sql" :kind :sql :class ,'sql-migration))
+    :documentation "Mapping between migration resource files and their respective classes"))
+  (:documentation "Provider for discovering migrations from a list of local paths"))
 
 (defmethod initialize-instance :after ((provider local-path-provider) &key)
   (unless (listp (provider-paths provider))
     (error "Must specify a list of migration resource paths")))
 
+(defmethod get-file-mapping ((provider local-path-provider) extension)
+  (find extension
+        (provider-file-mappings provider)
+        :key (lambda (item)
+               (getf item :extension))
+        :test #'string-equal))
+
 (defun make-provider (paths)
-  "Creates a local path provider using the given path"
+  "Creates a local path provider. Migration resources will be
+discovered from the given PATHS."
   (make-instance 'local-path-provider
                  :name "local-path"
                  :paths paths
@@ -118,7 +138,7 @@
   "Filters all files that match the given pattern scanner"
   (unless (uiop:directory-exists-p path)
     (error "path does not exist or is not a directory: ~a" path))
-  (log:debug "Discovering migration files from ~a" path)
+  (log:debug "[LP-PROVIDER] Discovering migration files from ~a" path)
   (let ((files (uiop:directory-files path)))
     (remove-if-not (lambda (file)
                      (migration-file-p file scanner))
@@ -127,20 +147,24 @@
 (defun group-migration-files-by-id (files scanner)
   "Groups migration files by id. Each group consists of the upgrade and downgrade scripts."
   (reduce (lambda (acc file)
-            (cl-ppcre:register-groups-bind (id description operation)
+            (cl-ppcre:register-groups-bind (id description direction extension)
                 (scanner (namestring file))
               (let* ((id (parse-integer id))
-                    (group (gethash id acc nil)))
+                     (group (gethash id acc nil)))
                 (setf (gethash id acc)
-                      (push (list :id id :operation operation :description description :path file)
+                      (push (list :id id
+                                  :direction direction
+                                  :description description
+                                  :path file
+                                  :extension extension)
                             group))))
             acc)
           files
-          :initial-value (make-hash-table)))
+          :initial-value (make-hash-table :test #'equal)))
 
-(defun find-migration-from-group (item sequence &key (indicator :operation) (test #'equal))
+(defun find-migration-from-group (item sequence &key (indicator :direction) (test #'equal))
   "Returns the migration spec from a group of migrations by searching for the item and indicator.
-One example usage of this function is to find a migration by operation (e.g. `up` or `down`)
+One example usage of this function is to find a migration by `direction' (e.g. `up' or `down')
 You can use this helper function to find migrations in the groups returned by the
 GROUP-MIGRATION-FILES-BY id function."
   (find item
@@ -157,17 +181,28 @@ GROUP-MIGRATION-FILES-BY id function."
                               "_"))
 
 (defmethod provider-list-migrations ((provider local-path-provider) &key)
-  (log:debug "Migration paths to be scanned: ~a" (local-path-provider-paths provider))
-  (let* ((paths (local-path-provider-paths provider))
-         (scanner (cl-ppcre:create-scanner *migration-file-regex*))
+  (log:debug "Migration paths to be scanned: ~a" (provider-paths provider))
+  (let* ((paths (provider-paths provider))
+         (scanner (cl-ppcre:create-scanner (provider-scan-pattern provider)))
          (files (mapcan (lambda (path) (find-migration-files path scanner)) paths))
          (groups (group-migration-files-by-id files scanner))
          (result nil))
     (maphash (lambda (k v)
-               (let ((id k)
-                     (up-migration (find-migration-from-group "up" v :indicator :operation))
-                     (down-migration (find-migration-from-group "down" v :indicator :operation)))
-                 (push (make-instance 'local-path-migration
+               (let* ((id k)
+                      (up-migration (find-migration-from-group "up" v :indicator :direction))
+                      (down-migration (find-migration-from-group "down" v :indicator :direction))
+                      (up-extension (getf up-migration :extension))
+                      (down-extension (getf down-migration :extension))
+                      (mapping (get-file-mapping provider up-extension))
+                      (migration-class (getf mapping :class)))
+                 (unless (equal up-extension down-extension)
+                   (error "Upgrade and downgrade migration for ~A are of different kinds: ~A and ~A"
+                          id up-extension down-extension))
+                 (unless mapping
+                   (error "No file mapping found for migration id ~A of kind ~A" id up-extension))
+                 (unless migration-class
+                   (error "No mapping class found for migration id ~A of kind ~A" id up-extension))
+                 (push (make-instance migration-class
                                       :id id
                                       :description (getf up-migration :description)
                                       :applied nil
@@ -178,8 +213,8 @@ GROUP-MIGRATION-FILES-BY id function."
     result))
 
 (defmethod provider-create-migration ((provider local-path-provider) &key id description up down)
-  (log:debug "Creating new migration in path ~a" (first (local-path-provider-paths provider)))
-  (let* ((provider-path (first (local-path-provider-paths provider)))
+  (log:debug "Creating new migration in path ~a" (first (provider-paths provider)))
+  (let* ((provider-path (first (provider-paths provider)))
          (id (or id (make-migration-id)))
          (description (normalize-description (or description "new migration")))
          (up-content (or up ""))
