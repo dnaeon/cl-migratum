77cc659ee20ef86291b85a3180411ac2ccd56126
Marin Atanasov Nikolov <dnaeon@gmail.com>
2022-03-28 10:55:36 +0300

    test/migrations: commit test Lisp code-based migration

---
+++ t/migrations/20220327224455-lisp_code_migration.down.lisp
@@ -0,0 +1,5 @@
+;; id: 20220327224455
+;; direction: DOWN
+;; description: lisp_code_migration
+
+(:system :cl-migratum.test :package :cl-migratum.test :handler :down-20220327224455)
---
+++ t/migrations/20220327224455-lisp_code_migration.up.lisp
@@ -0,0 +1,5 @@
+;; id: 20220327224455
+;; direction: UP
+;; description: lisp_code_migration
+
+(:system :cl-migratum.test :package :cl-migratum.test :handler :up-20220327224455)
