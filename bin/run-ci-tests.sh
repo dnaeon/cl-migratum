5f55e0597db5a10bb7935845b25d98a7081d4665
Marin Atanasov Nikolov <dnaeon@gmail.com>
2022-12-17 17:36:54 +0200

    Remove redundant test script

--- bin/run-ci-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-/usr/local/bin/install-quicklisp
-
-# configure ASDF to find our files in $WORKSPACE
-mkdir -p ~/.config/common-lisp/source-registry.conf.d/
-echo "(:tree \"${WORKSPACE}\")" > ~/.config/common-lisp/source-registry.conf.d/workspace.conf
-
-exec "${WORKSPACE}/entrypoint.sh" "$@"
