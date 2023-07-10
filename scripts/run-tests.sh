a1bd639e8e21a6d97199e423fae32163f19eeee9
Marin Atanasov Nikolov <dnaeon@gmail.com>
2023-07-10 12:48:52 +0300

    Update test pipelines

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -4,7 +4,18 @@ set -e
 
 LISP=${LISP:-sbcl}
 
-${LISP} --eval '(ql:quickload :cl-migratum.test)' \
+_no_debug_flag=""
+case "${LISP}" in
+    sbcl)
+        _no_debug_flag="--non-interactive"
+        ;;
+    ecl)
+        _no_debug_flag="--nodebug"
+        ;;
+esac
+
+${LISP} ${_no_debug_flag} \
+        --eval '(ql:quickload :cl-migratum.test)' \
         --eval '(setf rove:*enable-colors* nil)' \
         --eval '(asdf:test-system :cl-migratum.test)' \
         --eval '(uiop:quit (length (rove/core/stats:all-failed-assertions rove/core/stats:*stats*)))'
